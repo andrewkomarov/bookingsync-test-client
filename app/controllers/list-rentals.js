79b1f5901de8cdd5618789fcf51d29d4678a6cc2
Andrew Komarov <andrew@komarov.kiev.ua>
2017-10-24 06:23:55 -0400

    Added option to remove Rental

---
+++ app/controllers/list-rentals.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    removeRental(params) {
+      var rental_id = params.get('id');
+      this.get('store').findRecord('rental', rental_id, { backgroundReload: false }).then(function(rental) {
+      rental.deleteRecord();
+      //rental.get('isDeleted') ; // => true
+      rental.save();
+      });
+    }
+  }
+});
--- app/routes/list-rentals.js
+++ app/routes/list-rentals.js
@@ -3,16 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    
     return this.get('store').findAll('rental');
-/*
-  var that = this;
-  return new Ember.RSVP.Promise(function(resolve) {
-    that.store.findAll('rental').then(function(rentals) {
-      resolve(rentals.filterBy('isNew', false));
-    });
-  });
-*/
-
   }
 });
