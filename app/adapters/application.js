cbe6c4db567de6d7a5d191d0e4e455c5e8a46870
Andrew Komarov <andrew@komarov.kiev.ua>
2017-11-03 07:37:26 -0400

    Configure API server host in application adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://nuc.lan:3000',
+  host: 'http://komarov.kiev.ua:3000',
   crossDomain: true,
   headers: {
     'API_TOKEN': '3eb6db5a9026c547c72708438d496d942e976b252138db7e4e0ee5edd7539457d3ed0fa02ee5e7179420ce5290462018591adaf5f42adcf855da04877827def2',
